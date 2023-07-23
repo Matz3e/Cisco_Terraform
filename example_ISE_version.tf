87bfee9d8f503d7acfa2f163ef16f1604f220342
Matz3e <42473379+Matz3e@users.noreply.github.com>
2023-07-23 20:28:28 +0200

    Update example_ISE_version.tf

--- example_ISE_version.tf
+++ example_ISE_version.tf
@@ -1,3 +1,44 @@
+terraform {
+  required_providers {
+    ciscoise = {
+      source = "CiscoISE/ciscoise"
+      version = "0.6.21-beta"
+    }
+  }
+}
+
+
+# Configure provider with your  Cisco Identity Services Engine SDK credentials
+provider "ciscoise" {
+
+  #  Cisco Identity Services Engine user name / password
+  username = "admin"
+  password = "ISEc0ld"
+ 
+
+  #  Cisco Identity Services Engine base URL, FQDN or IP
+  base_url = "https://10.1.1.20"
+
+  # Boolean to enable debugging
+  debug = "false"
+
+  # Boolean to enable or disable SSL certificate verification
+  ssl_verify = "false"
+
+  # Boolean to enable or disable the usage of the ISE's API Gateway
+  use_api_gateway = "false"
+
+  # Boolean to enable or disable the usage of the X-CSRF-Token header
+  use_csrf_token = "false"
+
+  # Timeout (in seconds) for the RESTful HTTP requests
+  single_request_timeout = 60
+
+  # Boolean to enable or disable autoimport on resources
+  enable_auto_import = "false"
+
+}
+
 data "ciscoise_mnt_version" "example" {
   provider = ciscoise
 }
